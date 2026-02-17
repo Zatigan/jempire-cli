26fdf5ba3e9c634a732a821b05735c73869fba09
BOULET Pierre <pierre.boulet@maif.fr>
2026-02-17 15:50:03 +0100

    feat: creation of ressources variables

--- src/Game.java
+++ src/Game.java
@@ -22,5 +22,18 @@ public class Game {
 
   return result;
  }
- 
+
+ public static void game() {
+  int wood = 0;
+  int stone = 0;
+  int gold = 50;
+  int food = 100;
+  int villagers = 1;
+  boolean mine = false;
+  boolean castle = false;
+  int days = 1;
+  boolean peopleFed = true;
+  
+ }
+
 }
